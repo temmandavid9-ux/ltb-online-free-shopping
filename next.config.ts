ade8b895479717c91ec0cc3335156ed44c9c3262
Emmanuel David <temmandavid9@gmail.com>
2026-02-07 12:36:51 +0000

    https://pub-141831e61e69445289222976a15b6fb3.r2.dev/Image_to_url_V2/Chai

--- next.config.ts
+++ next.config.ts
@@ -40,6 +40,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'pub-141831e61e69445289222976a15b6fb3.r2.dev',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
