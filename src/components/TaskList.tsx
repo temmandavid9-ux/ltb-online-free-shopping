df3721fe614d659180e805349edd92edd6600969
Emmanuel David <temmandavid9@gmail.com>
2026-02-09 20:06:03 +0000

    add this on my instagram  link, CEO, EDEN 02

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,4 +1,3 @@
-
 'use client';
 import {
   useUser,
@@ -27,7 +26,7 @@ const TASK_DEFINITIONS = [
 
 const TASK_LINKS = {
   facebook: "https://facebook.com",
-  instagram: "https://instagram.com/eden02",
+  instagram: "https://instagram.com/ceo_eden_02",
   youtube: "https://youtube.com/@Eden-s8u",
   twitch: "https://twitch.tv/edenonlineshoppingstore"
 };
