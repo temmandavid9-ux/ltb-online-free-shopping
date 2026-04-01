49256d9aad62ff92e147cefbe1aef030c4161674
URIEL DAVID EMMANUEL <temmandavid9@gmail.com>
2026-04-01 15:02:59 +0100

    Fix: Made description optional in ImagePlaceholder type

--- src/lib/placeholder-images.ts
+++ src/lib/placeholder-images.ts
@@ -2,7 +2,7 @@ import data from './placeholder-images.json';
 
 export type ImagePlaceholder = {
   id: string;
-  description: string;
+  description?: string;
   imageUrl: string;
   imageHint: string;
 };
