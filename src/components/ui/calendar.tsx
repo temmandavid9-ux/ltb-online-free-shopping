23541121e111037c7a9e5967d7b0bdacb6fc6871
URIEL DAVID EMMANUEL <temmandavid9@gmail.com>
2026-04-01 14:13:14 +0100

    Emergency Fix: Removing custom calendar components to bypass build error

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -1,9 +1,7 @@
 "use client"
 
 import * as React from "react"
-import { ChevronLeft, ChevronRight } from "lucide-react"
 import { DayPicker } from "react-day-picker"
-
 import { cn } from "@/lib/utils"
 import { buttonVariants } from "@/components/ui/button"
 
@@ -53,13 +51,7 @@ function Calendar({
         day_hidden: "invisible",
         ...classNames,
       }}
-      // THIS IS THE FIX: Using 'Icon' instead of 'IconLeft'/'IconRight'
-      components={{
-        Icon: ({ ...props }) => {
-          if (props.name === "chevron-left") return <ChevronLeft className="h-4 w-4" />;
-          return <ChevronRight className="h-4 w-4" />;
-        },
-      }}
+      // We removed the 'components' block entirely to stop the Type Error
       {...props}
     />
   )
