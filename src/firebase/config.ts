50055ca3d49d45bf78b7933758fe8d7945615437
temmandavid9-ux <temmandavid9@gmail.com>
2026-04-06 19:08:52 +0100

    Update config.ts

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,6 +3,8 @@ export const firebaseConfig = {
   "appId": "1:1086489490325:web:3abdb3b9925017abba3c9c",
   "apiKey": "AIzaSyALXK5jsrUTMrKJPNXb4Q8Qz6Q5ULKWfGs",
   "authDomain": "studio-4748732424-9e222.firebaseapp.com",
+  // ADD THIS LINE BELOW:
+  "storageBucket": "studio-4748732424-9e222.firebasestorage.app",
   "measurementId": "",
   "messagingSenderId": "1086489490325"
 };
